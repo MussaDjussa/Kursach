8eecd02bc78822caddf69329c849ade0a4a9e7d7
superMussa <amushamid@mail.ru>
2022-02-08 21:27:24 +0300

    добавил авторизацию

--- Gudini/Pages/Login.xaml.cs
+++ Gudini/Pages/Login.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using BuckApp.Model;
 namespace BuckApp.Pages
 {
     /// <summary>
@@ -25,7 +16,7 @@ namespace BuckApp.Pages
             InitializeComponent();
         }
         /// <summary>
-        /// Navigation to StartUp Page
+        /// Навигация к стартовой странице
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -33,10 +24,76 @@ namespace BuckApp.Pages
         {
             NavigationService.Navigate(MainWindow.startUp);
         }
-
+        /// <summary>
+        /// Закрытие приложения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
+        /// <summary>
+        /// Кнопка войти
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // Проверка для пустые поля
+            if(string.IsNullOrWhiteSpace(login.Text) || string.IsNullOrWhiteSpace(password.Text))
+            {
+                MessageBox.Show("Остались пустые поля!!!", "Ошибка!!!");
+            }
+            else
+            {
+                if(UserLogin() != null)
+                {
+                    NavigationService.Navigate(MainWindow.startUp);
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь не найден!!!","Ошибка!!!");
+                }
+            }
+        }
+        /// <summary>
+        /// Проверка поля на латинские заглавные или строчные символы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Regex.IsMatch(login.Text, "[^A-z]"))
+            {
+                MessageBox.Show("Можно вводить только латинские заглавные или строчные символы!!!", "Ошибка!!!");
+                login.Text = login.Text.Remove(login.Text.Length - 1);
+                login.SelectionStart = login.Text.Length;
+            }
+        }
+        /// <summary>
+        /// Проверка поля на латинские заглавные и строчные символы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Regex.IsMatch(password.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Можно вводить только латинские заглавные или строчные символы!!!", "Ошибка!!!");
+                password.Text = password.Text.Remove(password.Text.Length - 1);
+                password.SelectionStart = password.Text.Length;
+            }
+        }
+        /// <summary>
+        /// Поиск пользователя по введеным полям
+        /// </summary>
+        /// <returns>user</returns>
+        private User UserLogin()
+        {
+            User findUser = MainWindow.model.User.ToList().Find(q=>q.Login == login.Text && q.Password == password.Text);
+            return findUser;
+        }
+        
     }
 }
